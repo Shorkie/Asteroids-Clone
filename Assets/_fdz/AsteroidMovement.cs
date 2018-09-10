9bebce6c0bc4b6e50c80d1d91f9688b6017d09bc
daniel <fdzextra@gmail.com>
2018-09-10 16:32:56 -0300

    added asteroid that spawn and move

---
+++ Assets/_fdz/AsteroidMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Name
+{
+	public class AsteroidMovement : MonoBehaviour
+	{
+		public Vector2 dir;
+		public float speed;
+
+		Rigidbody2D rb;
+
+		void Awake()
+		{
+			rb = GetComponent<Rigidbody2D>();
+		}
+
+		void FixedUpdate()
+		{
+			rb.MovePosition(rb.position + (dir.normalized * (speed * Time.deltaTime)));
+		}
+	}
+}
---
+++ Assets/_fdz/AsteroidSpawner.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Name
+{
+	public class AsteroidSpawner : MonoBehaviour
+	{
+		public Camera cam;
+		public Transform newParent;
+		public GameObject prefab;
+		public LayerMask layers;
+		public Transform player;
+
+		float camHeight;
+		float camWidth;
+		Vector2 margin;
+
+		float spawnRate = 1;
+		float timer;
+
+		void Update()
+		{
+			timer += Time.deltaTime;
+			if ( timer >= spawnRate )
+			{
+				timer = 0;
+				Spawn();
+			}
+		}
+
+		void Spawn()
+		{
+			var prefabIns = Instantiate(prefab);
+			prefabIns.transform.parent = newParent;
+
+			camHeight = cam.orthographicSize;
+ 			camWidth = camHeight * cam.aspect;
+
+			margin = prefabIns.GetComponent<SpriteRenderer>().size * prefabIns.transform.localScale;
+			Debug.Log("margin: " + margin.ToString());
+
+			SetPosition(prefabIns);
+			
+			prefabIns.transform.position = new Vector3(prefabIns.transform.position.x, prefabIns.transform.position.y, layers);
+
+			var asteroid = prefabIns.GetComponent<AsteroidMovement>();
+			asteroid.dir = GetRandomDir(prefabIns.transform.position);
+		}
+
+		Vector2 GetRandomDir(Vector3 from)
+		{
+			return player.position - from;
+		}
+
+		void SetPosition(GameObject prefabIns)
+		{
+			int iterationsCounter = 0;
+			bool isPositionFree = false;
+			while ( isPositionFree == false )
+			{
+				isPositionFree = true;
+
+				prefabIns.transform.localPosition = GetRandomPosition();
+				var overlapedColliders = Physics2D.OverlapCircleAll(prefabIns.transform.localPosition, margin.x, layers);
+				
+				Debug.DrawLine(prefabIns.transform.position - Vector3.up * margin.x, prefabIns.transform.position + Vector3.up * margin.x, Color.red, 1);
+				Debug.DrawLine(prefabIns.transform.position - Vector3.right * margin.x, prefabIns.transform.position + Vector3.right * margin.x, Color.red, 1);
+
+				foreach (var coll in overlapedColliders)
+				{
+					if ( coll.gameObject == prefabIns )
+					{
+						continue;
+					}
+					isPositionFree = false;
+					break;
+				}
+
+				iterationsCounter++;
+				if ( iterationsCounter > 10 )
+				{
+					Debug.LogError("force break");
+					break;
+				}
+			}
+		}
+
+		Vector3 GetRandomPosition()
+		{
+			Vector3 center = cam.transform.position;
+
+			float xMax = camWidth + margin.x;
+			float x = Random.Range( -xMax, xMax ) * 1.5f;
+			x = Mathf.Clamp(x, -xMax, xMax);
+			
+			Debug.DrawLine( new Vector3(-xMax, -100000, 0), new Vector3(-xMax, 100000, 0), Color.magenta, 10 );
+			Debug.DrawLine( new Vector3(xMax, -100000, 0), new Vector3(xMax, 100000, 0), Color.magenta, 10 );
+
+			float yMax = camHeight + margin.y;
+			float y = yMax;
+
+			Debug.DrawLine( new Vector3(-100000, -yMax, 0), new Vector3(100000, -yMax, 0), Color.magenta, 10 );
+			Debug.DrawLine( new Vector3(-100000, yMax, 0), new Vector3(100000, yMax, 0), Color.magenta, 10 );
+
+			if ( x <= -xMax + margin.x || x >= xMax - margin.x )
+			{
+				y = Random.Range( -yMax, yMax );
+			}
+			else
+			{
+				y = GetRandomSign() * ( yMax );
+			}
+
+			return new Vector3(x, y);
+		}
+
+		int GetRandomSign()
+		{
+			int sign = Random.Range(0, 2);
+			if (sign == 0)
+			{
+				sign = -1;
+			}
+			else
+			{
+				sign = 1;
+			}
+			//Debug.Log("sign: " + sign);
+			return sign;
+		}
+	}
+}
